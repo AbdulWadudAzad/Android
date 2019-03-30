fb00823b666ffb602fa125f0c82e601169508874
[AbdulWadudAzad] <[azadmiu37@gmail.com]>
2019-03-30 22:22:10 +0600

    add

---
+++ app/src/main/java/com/azad/profile/AnotherActivity.java
@@ -0,0 +1,18 @@
+package com.azad.profile;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.lang.annotation.Annotation;
+
+public class AnotherActivity {
+
+
+   Button btnAdd;
+
+   protected void onCreate(Bundle saveInstanceState){
+
+   }
+}
--- app/src/main/java/com/azad/profile/MainActivity.java
+++ app/src/main/java/com/azad/profile/MainActivity.java
@@ -1,13 +1,29 @@
 package com.azad.profile;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    Button btnAdd;
+    EditText name, pass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name=(EditText)findViewById(R.id.editText2);
+        pass=(EditText)findViewById(R.id.editText4);
+        btnAdd=(Button)findViewById(R.id.button);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder sb=new StringBuilder();
+            }
+        });
+
     }
 }
